669bbb91ff6aec30621e0b8090f04aaa42069c0d
NikolaRadivojevic97 <nikolarad97@gmail.com>
2018-03-13 15:32:37 +0100

    implementiran inetrfejs u okviru master grane

--- Menjacnica/src/specifikacijainterfejsa/Interfejs.java
+++ Menjacnica/src/specifikacijainterfejsa/Interfejs.java
@@ -5,7 +5,7 @@ import java.util.GregorianCalendar;
 import valuta.Valuta;
 
 public interface Interfejs {
-	public void dodajKurs(String naziv,String skraceni,double srednjiKurs,double prodajniKurs, double kupovniKurs, GregorianCalendar datum);
-	public void obrisiKurs(String naziv, GregorianCalendar datum);
+	public void dodajKurs(Valuta valuta);
+	public void obrisiKurs(Valuta valuta);
 	public Valuta vratiKurs(String naziv, GregorianCalendar datum);
 }
--- Menjacnica/src/valuta/Menjacnica.java
+++ Menjacnica/src/valuta/Menjacnica.java
@@ -7,20 +7,29 @@ import specifikacijainterfejsa.Interfejs;
 
 public class Menjacnica implements Interfejs{
 	LinkedList<Valuta> valute=new LinkedList<Valuta>();
-	
-	public void dodajKurs(String naziv, String skraceni, double srednjiKurs, double prodajniKurs, double kupovniKurs,GregorianCalendar datum) {
-		
-		
+	public void dodajKurs(Valuta valuta) {
+		if(valuta==null || valute.contains(valuta)) {
+			System.out.println("Unet je null objekat ili vec postoji takav kurs");
+		}else {
+		valute.add(valuta);
+		}
 	}
-
-	public void obrisiKurs(String naziv, GregorianCalendar datum) {
-		
+	public void obrisiKurs(Valuta valuta) {
+		if(valuta==null)
+			throw new RuntimeException("Uneti objekart je null");
+		valute.remove(valuta);
 		
 	}
 
 	public Valuta vratiKurs(String naziv, GregorianCalendar datum) {
-		
+		if(naziv==null || datum.after(new GregorianCalendar()))
+			throw new RuntimeException("naziv je prazan ili je datum posle trenutnog");
+		for(int i=0;i<valute.size();i++) {
+			if(valute.get(i).getNaziv().equals(naziv) && valute.get(i).getDatum().equals(datum))
+				return valute.get(i);
+		}
 		return null;
 	}
+
 
 }
